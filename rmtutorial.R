54a184c19c7b651b1ba922ff51a93f7889b57f57
Nicolas Gutierrez <nicogrm3107@gmail.com>
2022-11-13 19:13:02 +0100

    Practicing saving document as R code

---
+++ rmtutorial.R
@@ -0,0 +1,14 @@
+---
+##  title: "RM Tutorial"
+# output: 
+#  html_document: 
+ # keep_md: yes
+#date: "2022-11-10"
+
+---
+  #+ {r}
+## Insert your brilliant WORKING code here!
+x <- 6
+y <- 4
+x + y
+
